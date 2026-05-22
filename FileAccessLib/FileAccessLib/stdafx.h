12f2e81222c1792b8aba14d4571ecb928c120310
RichardChambers <rickchambers.geo@gmail.com>
2026-05-22 17:51:42 -0400

    added define for _CRT_SECURE_NO_WARNINGS to turn off security warnings to replace strcpy() with strcpy_s(), etc. These are optional parts of the C11 spec and not provided by all compilers.

--- FileAccessLib/FileAccessLib/stdafx.h
+++ FileAccessLib/FileAccessLib/stdafx.h
@@ -5,6 +5,9 @@
 
 #pragma once
 
+// Turn off the Microsoft security warnings.
+#define _CRT_SECURE_NO_WARNINGS
+
 #ifndef _WIN32_WINNT		// Allow use of features specific to Windows XP or later.                   
 #define _WIN32_WINNT 0x0501	// Change this to the appropriate value to target other versions of Windows.
 #endif						
